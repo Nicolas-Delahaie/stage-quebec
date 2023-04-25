5b23b43c438240d0c4eca2e6c247f7229f111ff5
Oier Cesat <oier.cesat@gmail.com>
2023-04-25 20:26:39 +0200

    initialisation du projet React

--- src/appli-react/src/App.js
+++ src/appli-react/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
+import Home from "./pages/Home/Home";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/appli-react/src/utils/styles/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+    DarkBlue: "#00296B",
+    MediumBlue: "#003F88",
+    LightBlue: "#00509D",
+    DarkYellow: "#FDC500",
+    LightYellow: "#FFD500",
+}
