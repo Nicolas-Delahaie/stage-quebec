58ada45658863cddeb07c4718d8f33cc0ee888a7
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-05-01 16:13:25 +0200

    Seeder fonctionnel

--- src/apis_laravel/database/seeders/DatabaseSeeder.php
+++ src/apis_laravel/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,25 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+use DateTime;
+
+use \App\Models\User;
+use \App\Models\TypeUtilisateur;
+use \App\Models\Departement;
+use \App\Models\Scenario;
+use \App\Models\Modification;
+use \App\Models\Proposer;
+use \App\Models\Enseigner;
+use \App\Models\Alouer;
+use \App\Models\Utilisateur;
+use \App\Models\Rdv;
+use \App\Models\Contrainte;
+use \App\Models\Cours;
+use \App\Models\Liberation;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +29,181 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
+        
+        // TABLES AVEC 0 DEPENDANCES
+        $type_utilisateur = [
+            ["nom" => "administrateur"],
+            ["nom" => "responsable"],
+            ["nom" => "professeur"]
+        ];
+        $cours = [
+            ["nom" => "Mathématiques"],
+            ["nom" => "Français"],
+            ["nom" => "Histoire"],
+            ["nom" => "Anglais"],
+            ["nom" => "Sciences physiques"],
+            ["nom" => "Sciences naturelles"],
+            ["nom" => "Informatique"],
+            ["nom" => "Géographie"],
+            ["nom" => "Économie"],
+            ["nom" => "Philosophie"],
+            ["nom" => "Arts"],
+            ["nom" => "Musique"],
+            ["nom" => "Théâtre"],
+            ["nom" => "Journalisme"],
+            ["nom" => "Marketing"],
+            ["nom" => "Design graphique"],
+            ["nom" => "Architecture"],
+            ["nom" => "Psychologie"],
+            ["nom" => "Sociologie"],
+            ["nom" => "Anthropologie"],
+            ["nom" => "Droit"],
+            ["nom" => "Médecine"],
+            ["nom" => "Ingénierie"],
+            ["nom" => "Commerce"]
+        ];
+        $liberation = [
+            ["motif" => "PVRTT"],
+            ["motif" => "Congé"],
+            ["motif" => "Gestion staiaires"],
+        ];
+        foreach($type_utilisateur as $val){TypeUtilisateur::create($val);}
+        foreach($cours as $val){Cours::create($val);}
+        foreach($liberation as $val){Liberation::create($val);}
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        // TABLES AVEC 1 DEPENDANCES
+        User::factory(20)->create();
+
+
+        // TABLES AVEC 2 DEPENDANCES
+        $enseigner=[];
+        foreach(Cours::all() as $cours){
+            //Chaque cours a un prof
+            array_push($enseigner, ["cours_id" => $cours->id, "professeur_id"=>$faker->numberBetween(1, User::count())]);
+        }
+        $alouer=[
+            ["tempsALoue"=>0.5125, "utilisateur_id"=>1, "liberation_id"=>3],
+            ["tempsALoue"=>0.823, "semestre"=>1, "utilisateur_id"=>3, "liberation_id"=>3],
+            ["tempsALoue"=>0.732, "annee"=>2023, "semestre"=>1, "utilisateur_id"=>6, "liberation_id"=>3],
+            ["tempsALoue"=>0.9, "annee"=>2023, "semestre"=>2, "utilisateur_id"=>7, "liberation_id"=>3],
+            ["tempsALoue"=>0.3, "annee"=>2024, "semestre"=>2, "utilisateur_id"=>2, "liberation_id"=>2],
+            ["tempsALoue"=>0.005, "annee"=>2024, "utilisateur_id"=>4, "liberation_id"=>2],
+            ["tempsALoue"=>0.2, "annee"=>2023, "semestre"=>2, "utilisateur_id"=>5, "liberation_id"=>2],
+            ["tempsALoue"=>0.125, "annee"=>2023, "semestre"=>1, "utilisateur_id"=>10, "liberation_id"=>2],
+            ["tempsALoue"=>0.19, "semestre"=>1, "utilisateur_id"=>8, "liberation_id"=>1],
+            ["tempsALoue"=>0.23, "annee"=>2023, "utilisateur_id"=>9, "liberation_id"=>1],
+        ];
+        $departement=[
+            ["nom" => "Biologie", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Education Physique", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Analyses Biomedicales", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "GTEA", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Soins Infirmiers", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Mathémathiques", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Chimie", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Physique", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Architecture", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Genie Mecanique", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Genie Electronique", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Sciences humaines", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Philisophie", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Techniques Travail Social", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Administration", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Informatique", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Arts Visuels", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Design Interieur", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Lettres", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+            ["nom" => "Langues Modernes", "coordonnateur_id"=>$faker->unique()->numberBetween(1, User::count()), "nbEleves"=>$faker->numberBetween(20, 300)],
+        ];
+
+        foreach($enseigner as $val){Enseigner::create($val);}
+        foreach($alouer as $val){Alouer::create($val);}
+        foreach($departement as $val){Departement::create($val);}
+
+
+        // TABLES AVEC 3 DEPENDANCES
+        $proposer = [];
+        foreach (Departement::all() as $departement) {
+            // On attribue a chaque departement entre 3 et 8 cours
+            
+            $coursProposes = [];
+            $nbCours = $faker->numberBetween(3, 8);
+            // On s'assure que les cours soient uniques
+            while (count($coursProposes) < $nbCours) {
+                $cours = $faker->numberBetween(1, Cours::count());
+                if (!in_array($cours, $coursProposes)){
+                    array_push($coursProposes, $cours);
+                }
+            }
+            
+            // On ajoute les cours proposes
+            foreach ($coursProposes as $cours){
+                array_push($proposer, [
+                    "cours_id" => $cours, 
+                    "departement_id" => $departement->id, 
+                    "ponderation" => $faker->numberBetween(1, 5),
+                    "tailleGroupes" => $faker->numberBetween(10, 35),
+                    "nbGroupes" => $faker->numberBetween(1, 4),
+                ]);
+            }
+            
+        }
+        $scenario = [];
+        foreach (Departement::all() as $departement) {
+            // On attribue a chaque departement entre 0 et 4 scenarios
+            
+            $nbScenarios = $faker->numberBetween(1, 6);
+            for ($i=1; $i < $nbScenarios; $i++) {
+                array_push($scenario, [
+                    "aEteValide" => true,
+                    "annee" => 2022 - $i,
+                    "departement_id" => $departement->id,
+                    "proprietaire_id" => $faker->numberBetween(1, User::count()),
+                ]);
+            }
+            
+            //On ajoute le dernier qui n est pas valide
+            array_push($scenario, [
+                "aEteValide" => false,
+                "annee" => 2022,
+                "departement_id" => $departement->id,
+                "proprietaire_id" => $faker->numberBetween(1, User::count()),
+            ]);
+        }
+
+        foreach($proposer as $val){Proposer::create($val);}
+        foreach($scenario as $val){Scenario::create($val);}
+
+
+        // TABLES AVEC 4 DEPENDANCES
+        $modification = [];
+        foreach (Departement::all() as $dept) {
+            // On attribue a chaque coordonnateur entre 0 et 4 modifications
+            $coordo = $dept->getCoordonnateur()->get()->first();
+
+            for ($i=0; $i < $faker->numberBetween(0, 4); $i++) {
+                array_push($modification, [
+                    "scenario_id" => $faker->numberBetween(1, Scenario::count()),
+                    "utilisateur_id" => $coordo->id,
+                    "date_modif" => $faker->dateTimeBetween($startDate = '2010-01-01', $endDate = 'now'),
+                ]);
+            }
+        }
+        $rdv = [];
+        foreach (Scenario::all() as $scenario){
+            // On attribue a chaque scenario entre 0 et 4 rendez-vous
+            for ($i=0; $i < $faker->numberBetween(0, 4); $i++) {
+                array_push($rdv, [
+                    "scenario_id" => $scenario->id,
+                    "jour" => $faker->dateTimeBetween($startDate = '20-04-'.$scenario->annee, $endDate = '20-09-'.$scenario->annee),
+                    "horaire" => $faker->numberBetween(8, 16).":".$faker->numberBetween(0,3)*15
+                ]);
+            }
+        }
+
+        foreach($modification as $val){Modification::create($val);}
+        foreach($rdv as $val){Rdv::create($val);}
     }
 }
