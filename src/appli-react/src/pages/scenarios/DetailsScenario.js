7b98fbc6f49266dc7d2d427909ee8e483bfaf460
Oier Cesat <oier.cesat@gmail.com>
2023-05-16 11:13:09 -0400

    Implémentation du style du scénario

--- src/appli-react/src/pages/scenarios/DetailsScenario.js
+++ src/appli-react/src/pages/scenarios/DetailsScenario.js
@@ -6,6 +6,9 @@ import { useEffect, useState } from "react";
 import { Loader, colors, fonts } from "../../utils/styles";
 
 import styled from "styled-components";
+import { useContext } from "react";
+import { AppContext } from "../../utils/context/context";
+
 
 /* ---------------------------------- STYLE --------------------------------- */
 
@@ -28,6 +31,28 @@ const DivDetailsScenario = styled.div`
     padding: 1rem;
 `;
 
+const DivTableau = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    overflow: auto;
+
+    ::-webkit-scrollbar {
+        height: 0.5rem;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${colors.jauneFonce};
+        border-radius: 1rem;
+    }
+`;
+
 const H1Scenario = styled.h1`
     font-size: 2rem;
     font-family: ${fonts.titre};
@@ -48,11 +73,61 @@ const H2Scenario = styled.h2`
     color: ${colors.bleuFonce};
 `;
 
+const TableScenario = styled.table`
+    margin: 1rem auto;
+    border-collapse: collapse;
+    border: 1px solid ${colors.bleuFonce};
+
+    overflow-x: scroll;
+`;
+
+const ThScenario = styled.th`
+    font-size: 0.9rem;
+    font-family: ${fonts.titre};
+    color: ${colors.bleuFonce};
+    padding: 0.5rem;
+    background-color: ${colors.jauneFonce};
+    border: 1px solid ${colors.bleuFonce};
+`;
+
+const TrScenario = styled.tr`
+    font-size: 0.9rem;
+    font-family: ${fonts.texte};
+    border-collapse: collapse;
+    border: 1px solid ${colors.bleuFonce};
+    /*&:nth-child(even) {
+        background-color: ${colors.gris};
+    }
+    &:nth-child(odd) {
+        background-color: ${colors.grisClair};
+    }*/
+    &:hover {
+        background-color: ${colors.jauneTresClair};
+    }
+`;
+
+const TdScenario = styled.td`
+    font-size: 0.9rem;
+    padding: 0.25rem;
+    font-family: ${fonts.texte};
+    boder-collapse: collapse;
+    border: 1px solid ${colors.bleuFonce};
+    text-align: center;
+    &:nth-child(-n+4) {
+        font-family: ${fonts.titre};
+        background-color: ${colors.grisClair};
+    }
+    &:hover {
+        background-color: ${colors.jauneClair};
+    }
+`;
+
 function DetailsScenario() {
     const id = useParams().id;
     const [scenario, setScenario] = useState({});
     const [modification, setModification] = useState({});
     const [loading, setLoading] = useState(false);
+    const { getToken } = useContext(AppContext);
 
 
     /**
@@ -60,7 +135,14 @@ function DetailsScenario() {
      */
     useEffect(() => {
         setLoading(true);
-        fetch(`http://localhost:8000/api/scenarios/${id}/detaille`)
+        fetch(`http://localhost:8000/api/scenarios/${id}/detaille`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + getToken().slice(1, -1),
+
+            }
+        })
             .then((response) => {
                 return response.json();
             })
@@ -76,7 +158,14 @@ function DetailsScenario() {
 
     useEffect(() => {
         setLoading(true);
-        fetch(`http://localhost:8000/api/scenarios/${id}/modifications`)
+        fetch(`http://localhost:8000/api/scenarios/${id}/modifications`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + getToken().slice(1, -1),
+
+            }
+        })
             .then((response) => {
                 return response.json();
             })
@@ -102,7 +191,7 @@ function DetailsScenario() {
                     <p>Date de création : {scenario.created_at}</p>
                     <p>Dernière modification : {scenario.updated_at}</p>
                     <H2Scenario>Propriétaire : {scenario.proprietaire.nom}</H2Scenario>
-                    <H1Scenario>Historique des modifications : </H1Scenario>
+                    <H1Scenario>Historique des modifications</H1Scenario>
                     {
                         modification[0] === undefined ? (
                             <p>Aucune modification n'a été apportée</p>
@@ -119,6 +208,83 @@ function DetailsScenario() {
                             </div>
                         )
                     }
+                    <H1Scenario>Le scénario </H1Scenario>
+                    <DivTableau>
+                    <TableScenario>
+                        <thead>
+                            <TrScenario>
+                                <ThScenario>Titre du cours</ThScenario>
+                                <ThScenario>Pondération</ThScenario>
+                                <ThScenario>Nombre d'élèves</ThScenario>
+                                <ThScenario>Nombre de groupes</ThScenario>
+                                <ThScenario>Professeur 1</ThScenario>
+                                <ThScenario>Professeur 2</ThScenario>
+                                <ThScenario>Professeur 3</ThScenario>
+                                <ThScenario>Professeur 4</ThScenario>
+                                <ThScenario>Professeur 5</ThScenario>
+                                <ThScenario>Professeur 6</ThScenario>
+                                <ThScenario>Professeur 7</ThScenario>
+                                <ThScenario>Professeur 8</ThScenario>
+                                <ThScenario>Professeur 9</ThScenario>
+                                <ThScenario>Professeur 10</ThScenario>
+                                <ThScenario>Professeur 11</ThScenario>
+                            </TrScenario>
+                        </thead>
+                        <tbody>
+                            <TrScenario>
+                                <TdScenario>cours 1</TdScenario>
+                                <TdScenario>3</TdScenario>
+                                <TdScenario>30</TdScenario>
+                                <TdScenario>1</TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                            </TrScenario>
+                            <TrScenario>
+                                <TdScenario>cours 2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>28</TdScenario>
+                                <TdScenario>3</TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario></TdScenario>
+                            </TrScenario>
+                            <TrScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario>Calcul de la CI</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario></TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario>2</TdScenario>
+                                <TdScenario></TdScenario>
+                            </TrScenario>
+                        </tbody>
+                    </TableScenario>
+                    </DivTableau>
                 </DivDetailsScenario>
             )}
         </DivPageDetailsScenario>
