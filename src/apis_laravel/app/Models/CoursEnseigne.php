1bd932ae9f4f0569825705eb3a9fab3fa469f089
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-06-06 16:40:56 -0400

    Suppression des modèles supprimés + Creation du modele CoursEnseigne pour le seeder

---
+++ src/apis_laravel/app/Models/CoursEnseigne.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CoursEnseigne extends Model
+{
+    use HasFactory;
+    public $timestamps = false; //Pour que le seeder sache qu'il ne faut pas l initialiser (meme s il n existe pas)
+    protected $table = 'cours_enseigne';
+}
